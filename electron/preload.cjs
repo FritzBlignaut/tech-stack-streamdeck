8d558b7c91e73b289b64eb6836be56b58fa1a380
Fritz <fritz.blignaut@gmail.com>
2026-05-19 16:05:01 +1000

    feat: add functionality to open applications and browse for files via IPC

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -3,13 +3,15 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('streamDeck', {
-  onInfo:       (cb) => ipcRenderer.on('deck:info',  (_, data) => cb(data)),
-  onKeyDown:    (cb) => ipcRenderer.on('deck:down',  (_, data) => cb(data)),
-  onKeyUp:      (cb) => ipcRenderer.on('deck:up',    (_, data) => cb(data)),
-  onSleep:      (cb) => ipcRenderer.on('deck:sleep', (_, data) => cb(data)),
-  onWake:       (cb) => ipcRenderer.on('deck:wake',  (_, data) => cb(data)),
+  onInfo:    (cb) => { const wrap = (_, d) => cb(d); ipcRenderer.on('deck:info',  wrap); return () => ipcRenderer.removeListener('deck:info',  wrap) },
+  onKeyDown: (cb) => { const wrap = (_, d) => cb(d); ipcRenderer.on('deck:down',  wrap); return () => ipcRenderer.removeListener('deck:down',  wrap) },
+  onKeyUp:   (cb) => { const wrap = (_, d) => cb(d); ipcRenderer.on('deck:up',    wrap); return () => ipcRenderer.removeListener('deck:up',    wrap) },
+  onSleep:   (cb) => { const wrap = (_, d) => cb(d); ipcRenderer.on('deck:sleep', wrap); return () => ipcRenderer.removeListener('deck:sleep', wrap) },
+  onWake:    (cb) => { const wrap = (_, d) => cb(d); ipcRenderer.on('deck:wake',  wrap); return () => ipcRenderer.removeListener('deck:wake',  wrap) },
   setButtonIcon: (index, rgbaData) => ipcRenderer.invoke('button:setIcon', { index, rgbaData }),
   saveProfile:    (data)        => ipcRenderer.invoke('profile:save', data),
   loadProfile:    ()            => ipcRenderer.invoke('profile:load'),
-  executeHotkey:  (keys)        => ipcRenderer.invoke('action:hotkey', { keys }),
+  executeHotkey:    (keys)          => ipcRenderer.invoke('action:hotkey',   { keys }),
+  openApplication:  (target, mode)  => ipcRenderer.invoke('action:open-app', { target, mode }),
+  browseForFile:    ()              => ipcRenderer.invoke('dialog:open-file'),
 })
